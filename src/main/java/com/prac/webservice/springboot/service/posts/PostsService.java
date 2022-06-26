cf78a743bd61e62f686c600befe07a7cdba2ab83
Thovy <kingvely151113@gmail.com>
2022-06-27 00:44:05 +0900

    :hammer: Feat: Add scripts to CRUD and CRUD Test

---
+++ src/main/java/com/prac/webservice/springboot/service/posts/PostsService.java
@@ -0,0 +1,21 @@
+package com.prac.webservice.springboot.service.posts;
+
+import com.prac.webservice.springboot.domain.posts.PostsRepository;
+import com.prac.webservice.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+
+    }
+}
