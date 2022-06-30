e20eca13c02b0414483d83c71ea76f80590a3e08
Thovy <kingvely151113@gmail.com>
2022-06-30 15:32:02 +0900

    :hammer: Feat: Add script to findAll

--- src/main/java/com/prac/webservice/springboot/service/posts/PostsService.java
+++ src/main/java/com/prac/webservice/springboot/service/posts/PostsService.java
@@ -2,13 +2,16 @@ package com.prac.webservice.springboot.service.posts;
 
 import com.prac.webservice.springboot.domain.posts.Posts;
 import com.prac.webservice.springboot.domain.posts.PostsRepository;
+import com.prac.webservice.springboot.web.dto.PostsListResponseDto;
 import com.prac.webservice.springboot.web.dto.PostsResponseDto;
 import com.prac.webservice.springboot.web.dto.PostsSaveRequestDto;
 import com.prac.webservice.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -37,5 +40,12 @@ public class PostsService {
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
