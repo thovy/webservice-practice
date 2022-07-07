f32e5c0ff812908563292cb6f63c9534226e472f
Thovy <kingvely151113@gmail.com>
2022-07-08 00:53:59 +0900

    :hammer: Feat: Add JpaConfig.java

--- src/main/java/com/prac/webservice/springboot/Application.java
+++ src/main/java/com/prac/webservice/springboot/Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing  // JPA Auditing 활성화
+// @EnableJpaAuditing  // JPA Auditing 활성화 //테스트를 위해 EnableJpaAuditing 삭제
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
